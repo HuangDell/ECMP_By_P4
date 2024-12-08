93ef4fb56c8f75e5525650ddac6eb4b275cfeb7d
Dell <2429368752@qq.com>
2024-12-08 03:09:19 +0000

    update control plane configure

--- src/cp/setup.py
+++ src/cp/setup.py
@@ -67,24 +67,14 @@ def add_port_config(port_config):
 
 
 def add_l2_forward(forward_configs):
-    l2_forward = bfrt.let_it_flow.pipe.SwitchIngress.random_forward
+    l2_forward = bfrt.ecmp.pipe.SwitchIngress.ecmp_forward
     def generate_random_port_forward(dst_addr,port_begin,port_end):
         for i in range(port_begin,port_end+1):
             l2_forward.add_with_forward(dst_addr=dst_addr,port_index=i-port_begin,port=i)
-    def generate_exact_port_forward(dst_addr,exact_port):
-        for i in range(4):
-            l2_forward.add_with_forward(dst_addr=dst_addr,port_index=i,port=exact_port)
     
     for config in forward_configs:
-        if config[1]==config[2]:     # exact l2 forward
-            generate_exact_port_forward(config[0],config[1])
-        else:
             generate_random_port_forward(*config)
 
-def add_exact_forward(forward_configs):
-    l2_forward = bfrt.let_it_flow.pipe.SwitchIngress.exact_forward
-    for config in forward_configs:
-            l2_forward.add_with_forward(dst_addr=config[0],port=config[1])
     
 
 def add_arp(dev_ports):
